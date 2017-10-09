17e3306a7c79b259d90d4b942fe0da1ffd8adcf3
Schäfer <SchaeferC@wifi-it.at>
2017-10-09 11:23:08 +0200

    COMMIT 20171009 1122

--- inc/select-todos.sql
+++ inc/select-todos.sql
@@ -7,9 +7,26 @@
 SELECT t.text, t.active, u.name, GROUP_CONCAT(c.name ORDER BY c.name DESC SEPARATOR ', ') AS catname 
 FROM todos as t
 /* Lookup Table */
-INNER JOIN todos_categories AS tc ON t.todos_id = tc.todos_id
+LEFT JOIN todos_categories AS tc ON t.todos_id = tc.todos_id
 /* Categories name auslesen */
-INNER JOIN categories AS c ON tc.categories_id = c.categories_id
+LEFT JOIN categories AS c ON tc.categories_id = c.categories_id
 /* User Name auslesen, verknüft mit todos! */
 INNER JOIN user AS u ON t.user_id = u.user_id
 GROUP BY text
+
+/*
+
+
+*/
+
+CREATE VIEW custormersOrders AS 
+    SELECT d.orderNumber,
+           d.orderName,
+           SUM(quantityOrdered * priceEach) As total
+    From 
+        orderDeteils As d 
+    INNER JOIN orders As o ON o.orderNumber = d.orderNumber
+    INNER JOIN 
+        custormers As c ON c.custormerNumber = o.sustormerNumber
+    GROUP BY d.orderNumber
+    ORDER BY total DESC;
